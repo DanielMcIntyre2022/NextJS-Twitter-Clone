24d2de28dbe7ed48c2920c50fef193cadbf57c9f
Daniel <daniel-mcintyre@hotmail.com>
2022-09-18 16:09:43 -0400

    initial commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+    BellIcon,
+    HashtagIcon,
+    CollectionIcon,
+    BookmarkIcon, 
+    DotCircleHorizontalIcon,
+    MailIcon, 
+    UserIcon,
+    HomeIcon
+} from '@heroicons/react/outline';
+
+function Sidebar() {
+  return (
+    <div>I am a sidebar</div>
+  )
+}
+
+export default Sidebar
