e1b4feefeb7e1d143001d8d0f3186ef736434716
Daniel <daniel-mcintyre@hotmail.com>
2022-09-18 16:27:26 -0400

    initial sidebar setup

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,10 +9,19 @@ import {
     UserIcon,
     HomeIcon
 } from '@heroicons/react/outline';
+import SidebarRow from './SidebarRow';
 
 function Sidebar() {
   return (
-    <div>I am a sidebar</div>
+    <div>
+        <img className="h-10 w-10" src="https://links.papareact.com/drq" alt=""/>
+        <SidebarRow Icon={HomeIcon} title={'Home'}/>
+        <SidebarRow Icon={HashtagIcon} title={'Explore'}/>
+        <SidebarRow Icon={BellIcon} title={'Notifications'}/>
+        <SidebarRow Icon={MailIcon} title={'Messages'}/>
+        <SidebarRow Icon={BookmarkIcon} title={'Bookmarks'}/>
+        <SidebarRow Icon={CollectionIcon} title={'Lists'}/>
+    </div>
   )
 }
 
