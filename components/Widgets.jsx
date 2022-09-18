9d6092dc9c2ed9545855108615f9d070a4f9e5e9
Daniel <daniel-mcintyre@hotmail.com>
2022-09-18 17:41:01 -0400

    improved responsiveness and UI for mobile, tablet and desktop

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -4,14 +4,14 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed';
 
 function Widgets() {
   return (
-    <div className='mt-2 px-2'>
+    <div className='col-span-2 mt-2 hidden lg:inline px-2'>
         <div className='flex item-center space-x-2 bg-gray-100 p-3 mt-2'>
             <SearchIcon className='h-5 w-5 text-gray-400'/>
             <input type="text" placeholder="Search Twitter" className='flex-1 outline-none bg-transparent rounded-full'/>
         </div>
         <TwitterTimelineEmbed
             sourceType="profile"
-            screenName="saurabhnemade"
+            screenName="DanielMcIntyree"
             options={{height: 400}}
         />
     </div>
