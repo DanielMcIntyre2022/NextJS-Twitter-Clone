ff07a160e96de89157fecbea28b3bf62475c2e08
Daniel <daniel-mcintyre@hotmail.com>
2022-09-21 11:06:46 -0400

    added styles for tweetbox component

--- components/TweetBox.jsx
+++ components/TweetBox.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
-// import {
-//     CalenderIcon, 
-//     EmojiHappyIcon,
-//     LocationMarkerIcon,
-//     SearchCircleIcon,
-// } from '@heroicons/react/outline'
+import {
+    CalenderIcon, 
+    EmojiHappyIcon,
+    LocationMarkerIcon,
+    SearchCircleIcon,
+    PhotographIcon
+} from '@heroicons/react/outline';
 
 function Tweetbox() {
   return (
-    <div>
+    <div className='flex space-x-2 p-5'>
         <img className="h-14 w-14 rounded-full object-cover mt-4" 
         src="https://links.papareact.com/gll"/>
-        <div>
-            <form>
-                <input type="text" placeholder="What's Happening?"/>
-                <div>
-                    <div>
-                        {/* <SearchCircleIcon className='h5 w-5'/>
-                        <EmojiHappyIcon className='h5 w-5'/>
-                        <CalenderIcon className='h5 w-5'/>
-                        <LocationMarkerIcon className='h5 w-5'/>
-                        <SearchCircleIcon className='h5 w-5'/> */}
+
+        <div className='flex flex-1 items-center pl-2'>
+            <form className='flex flex-1 flex-col'>
+                <input type="text" placeholder="What's Happening?" className='h-24 w-full text-xl outline-none placeholder:text-xl'/>
+                <div className='flex items-center'>
+                    <div className='flex flex-1 space-x-2 text-twitter'>
+                        <PhotographIcon className='h5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
+                        <SearchCircleIcon className='h5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
+                        <EmojiHappyIcon className='h5- w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
+                        <LocationMarkerIcon className='h5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
                     </div>
-                    <button>Tweet</button>
+                    <button className='bg-twitter px-5 py-2 font-bold text-white rounded-full'>Tweet</button>
                 </div>
             </form>
         </div>
@@ -30,4 +31,4 @@ function Tweetbox() {
   )
 }
 
-export default Tweetbox;
+export default Tweetbox
